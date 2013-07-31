4b71730d29c862e2bccf30ee772bc6d1f74c42c4
hengchu <hengchu.zhang@gmail.com>
2013-07-31 17:59:52 -0400

    modified paperfold screen category to change the screencapture size

--- PaperFold/UIView+Screenshot.m
+++ PaperFold/UIView+Screenshot.m
@@ -39,6 +39,7 @@
 
 - (UIImage*)screenshotWithOptimization:(BOOL)optimized
 {
+    CGRect rect = [[UIScreen mainScreen] bounds];
     if (optimized)
     {
         // take screenshot of the view
@@ -47,24 +48,24 @@
             if ([[[UIDevice currentDevice] systemVersion] floatValue]>=6.0)
             {
                 // in iOS6, there is no problem using a non-retina screenshot in a retina display screen
-                UIGraphicsBeginImageContextWithOptions(self.frame.size, NO, 1.0);
+                UIGraphicsBeginImageContextWithOptions(rect.size, NO, 1.0);
             }
             else
             {
                 // if the view is a mapview in iOS5.0 and below, screenshot has to take the screen scale into consideration
                 // else, the screen shot in retina display devices will be of a less detail map (note, it is not the size of the screenshot, but it is the level of detail of the screenshot
-                UIGraphicsBeginImageContextWithOptions(self.frame.size, NO, 0.0);
+                UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0.0);
             }
         }
         else
         {
             // for performance consideration, everything else other than mapview will use a lower quality screenshot
-            UIGraphicsBeginImageContext(self.frame.size);
+            UIGraphicsBeginImageContext(rect.size);
         }
     }
     else
     {
-        UIGraphicsBeginImageContextWithOptions(self.frame.size, NO, 0.0);
+        UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0.0);
     }
     
     
